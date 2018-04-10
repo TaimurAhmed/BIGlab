11a376eaeabf42642de50436970f06f590283a9a
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2018-04-10 20:49:20 +0100

    house keeping

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -32,70 +32,71 @@ var mediumBreak = "<break strength='medium' />";
 
 
 
-var handlers = {
-                "LaunchRequest": function () {
-                  memo = "The app was just launched!";
-                  score = 0;
-                  arrayPosition = 0;
-                  this.response.speak(
-                                      "Hi, i am quiz rhino ! " + mediumBreak + 
-                                      "I can help you revise American History from the 1920's ." +
-                                      "If you'd like that, then just say please test my knowledge ? "
-                                      ).listen(
-                                                "Let me repeat myself. If you'd like me to test your " +
-                                                "American History knowledge, then just say 'please "+
-                                                "test my knowledge'?"
-                                              ); 
-                  this.emit(':responseReady');
-                },"AMAZON.StopIntent": function () {
-                    memo = "You just stopped the app";
-                    arrayPosition = 0;
-                    this.response.speak("Ok hope you had fun, goodbye!"); // add condition to leave or pause?
-                    this.emit(':responseReady');
-                },"QuestionIntent": function () {//Yes everyone is well
-                  memo = "You are on question number " + numberOfQuestions;
-                  if(numberOfQuestions > arrayPosition){
-                      var say = questionArray[arrayPosition];
-                      arrayPosition++;
-                  }else{
-                      var say = "We have run out of flash cards for now! There were only " + numberOfQuestions+ ". I shall reset the questions and score,so that we can start again or you could ask me to stop for now.";
-                      arrayPosition = 0;
-                      score = 0;
-                  }
-                      this.response.speak(say).listen("I will repeat that again . . ."+say); 
-                      this.emit(':responseReady');
-                },
-                  "SessionEndedRequest": function () {
-                      console.log(`Session ended in help state: ${this.event.request.reason}`);
-                },"TrueFactIntent": function () {//If true
-                    memo = "You just answered true to the last question";
-                    var lastQuestion = arrayPosition - 1;
-                    var say;
-                    if(answerArray[lastQuestion] == true){
-                      var say = "That is correct, that was true";
-                      score++;
-                    }else{
-                      var say = "That is incorrect, that was actually false";
-                    }
-                    this.response.speak(say); 
-                    this.emit(':responseReady');
-                },"FalseFactIntent": function () {//If true
-                    memo = "You just answered true to the last question";
-                    var lastQuestion = arrayPosition - 1;
-                    var say;
-                    if(answerArray[lastQuestion] == false){
-                      var say = "That is correct, that was false";
-                      score++;
-                    }else{
-                      var say = "That is incorrect, that was actually true";
-                    }
-                    this.response.speak(say); 
-                    this.emit(':responseReady');
-                },"ScoreIntent": function () {//If true
-                    this.response.speak("your score is "+score); 
-                    this.emit(':responseReady');
-                }
-               };
+var handlers = 
+   {
+    "LaunchRequest": function () {
+      memo = "The app was just launched!";
+      score = 0;
+      arrayPosition = 0;
+      this.response.speak(
+                          "Hi, i am quiz rhino ! " + mediumBreak + 
+                          "I can help you revise American History from the 1920's ." +
+                          "If you'd like that, then just say please test my knowledge ? "
+                          ).listen(
+                                    "Let me repeat myself. If you'd like me to test your " +
+                                    "American History knowledge, then just say 'please "+
+                                    "test my knowledge'?"
+                                  ); 
+      this.emit(':responseReady');
+    },"AMAZON.StopIntent": function () {
+        memo = "You just stopped the app";
+        arrayPosition = 0;
+        this.response.speak("Ok hope you had fun, goodbye!"); // add condition to leave or pause?
+        this.emit(':responseReady');
+    },"QuestionIntent": function () {//Yes everyone is well
+        memo = "You are on question number " + numberOfQuestions;
+        if(numberOfQuestions > arrayPosition){
+            var say = questionArray[arrayPosition];
+            arrayPosition++;
+        }else{
+            var say = "We have run out of flash cards for now! There were only " + numberOfQuestions+ ". I shall reset the questions and score,so that we can start again or you could ask me to stop for now.";
+            arrayPosition = 0;
+            score = 0;
+        }
+            this.response.speak(say).listen("I will repeat that again . . ."+say); 
+            this.emit(':responseReady');
+    },
+      "SessionEndedRequest": function () {
+          console.log(`Session ended in help state: ${this.event.request.reason}`);
+    },"TrueFactIntent": function () {//If true
+        memo = "You just answered true to the last question";
+        var lastQuestion = arrayPosition - 1;
+        var say;
+        if(answerArray[lastQuestion] == true){
+          var say = "That is correct, that was true";
+          score++;
+        }else{
+          var say = "That is incorrect, that was actually false";
+        }
+        this.response.speak(say); 
+        this.emit(':responseReady');
+    },"FalseFactIntent": function () {//If true
+        memo = "You just answered true to the last question";
+        var lastQuestion = arrayPosition - 1;
+        var say;
+        if(answerArray[lastQuestion] == false){
+          var say = "That is correct, that was false";
+          score++;
+        }else{
+          var say = "That is incorrect, that was actually true";
+        }
+        this.response.speak(say); 
+        this.emit(':responseReady');
+    },"ScoreIntent": function () {//If true
+        this.response.speak("your score is "+score); 
+        this.emit(':responseReady');
+    }
+   };
 
 // This is the function that AWS Lambda calls every time Alexa uses your skill.
 exports.handler = function(event, context, callback){
