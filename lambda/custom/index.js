c33833398621d1ec40e43911e60ce3784a394046
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2018-04-26 16:33:09 +0100

    abstract questions to seperate class

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -4,16 +4,9 @@
 
 /*monkey*/
 
-const Alexa = require("alexa-sdk"); 
-
-
-
-const questionArray = [
-                      "The American President in 1928 was Herbert Hoover ? " ,
-                      "The Wall Street Crash happened in 1941. " ,
-                      "The Democrats won the elections in 1932. "
-                    ];
-
+const Alexa = require("alexa-sdk");
+const question = require("./question.js");
+const questionArray = question.returnAllQuestions();
 const numberOfQuestions = questionArray.length;
 
 const answerArray = [
--- lambda/custom/question.js
+++ lambda/custom/question.js
@@ -2,15 +2,13 @@
 
 module.exports = {
 
-        return{
-            const questionArray = [
-            "The American President in 1928 was "
-            + "Herbert Hoover ? " ,
-            "The Wall Street Crash happened in "
-            + "1941. " ,
-            "The Democrats won the elections in "
-            + "1932. "
-            ];
+    returnAllQuestions: function(){
+        const questionArray = [
+                      "The American President in 1928 was Herbert Hoover ? " ,
+                      "The Wall Street Crash happened in 1941. " ,
+                      "The Democrats won the elections in 1932. "
+                    ];
+        return questionArray;
+    }
 
-        }
-    };
+};
