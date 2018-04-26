f8475df622dc8f0937bdce5c841a391d4bd8f556
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2018-04-26 21:50:16 +0100

    create prompt obj factory

---
+++ lambda/custom/prompts.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    returnQuestionPrompts: function() {
+        const promptArray = [
+            "Is the statement true or false ? ",
+            "Is the statement correct or incorrect ? " +
+            "Yes or no ? "
+        ];
+        return promptArray;
+    }
+};
