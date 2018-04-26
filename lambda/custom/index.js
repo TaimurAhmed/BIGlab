37fba8cca922c962cfd37b8833a4b050101b5d43
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2018-04-26 16:39:43 +0100

    fixed untracked files

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,7 +2,7 @@
 /*jshint esversion: 6 */
 /* jshint node: true */
 
-/*monkey*/
+/*monkeyfsafsa*/
 
 const Alexa = require("alexa-sdk");
 const question = require("./question.js");
@@ -10,100 +10,102 @@ const questionArray = question.returnAllQuestions();
 const numberOfQuestions = questionArray.length;
 
 const answerArray = [
-                      true,
-                      false,
-                      true
-                    ];
+  true,
+  false,
+  true
+];
 
 const possibleUserResponsesArray = [
-                                    "Is the statement true or false ? ",
-                                    "Is the statement correct or incorrect ? " +
-                                    "Yes or no ? "
-                                  ];
+  "Is the statement true or false ? ",
+  "Is the statement correct or incorrect ? " +
+  "Yes or no ? "
+];
 
 // To leave a note of where the application was
 var memo = "The program just started. This shouldnt happen.";
-var say  = memo;
+var say = memo;
 var arrayPosition = 0;
 var score = 0;
 var ssmlMediumBreak = "<break strength='medium' />";
 
 
 
-var handlers = 
-   {
-    "LaunchRequest": function () {
-      memo = "You just launched the app!";
-      score = 0;
+var handlers = {
+  "LaunchRequest": function() {
+    memo = "You just launched the app!";
+    score = 0;
+    arrayPosition = 0;
+    this.response.speak(
+      "Hi, i am quiz rhino ! " + ssmlMediumBreak +
+      "I can help you revise American History from the 1920's ." +
+      "If you'd like that, then just say please test my knowledge ? "
+    ).listen(
+      "Let me repeat myself. If you'd like me to test your " +
+      "American History knowledge, then just say 'please " +
+      "test my knowledge'?"
+    );
+    this.emit(':responseReady');
+  },
+  "AMAZON.StopIntent": function() {
+    memo = "You just stopped the app!";
+    arrayPosition = 0;
+    this.response.speak("Ok hope you had fun, goodbye!"); // add condition to leave or pause?
+    this.emit(':responseReady');
+  },
+  "QuestionIntent": function() { //Yes everyone is well
+    memo = "You are on question number " + numberOfQuestions;
+    if (numberOfQuestions > arrayPosition) {
+      var say = questionArray[arrayPosition];
+      arrayPosition++;
+    } else {
+      var say = "We have run out of flash cards for now! There were only " + numberOfQuestions + ". I shall reset the questions and score,so that we can start again or you could ask me to stop for now.";
       arrayPosition = 0;
-      this.response.speak(
-                          "Hi, i am quiz rhino ! " + ssmlMediumBreak + 
-                          "I can help you revise American History from the 1920's ." +
-                          "If you'd like that, then just say please test my knowledge ? "
-                          ).listen(
-                                    "Let me repeat myself. If you'd like me to test your " +
-                                    "American History knowledge, then just say 'please "+
-                                    "test my knowledge'?"
-                                  ); 
-      this.emit(':responseReady');
-    },"AMAZON.StopIntent": function () {
-        memo = "You just stopped the app!";
-        arrayPosition = 0;
-        this.response.speak("Ok hope you had fun, goodbye!"); // add condition to leave or pause?
-        this.emit(':responseReady');
-    },"QuestionIntent": function () {//Yes everyone is well
-        memo = "You are on question number " + numberOfQuestions;
-        if(numberOfQuestions > arrayPosition){
-            var say = questionArray[arrayPosition];
-            arrayPosition++;
-        }else{
-            var say = "We have run out of flash cards for now! There were only " + numberOfQuestions+ ". I shall reset the questions and score,so that we can start again or you could ask me to stop for now.";
-            arrayPosition = 0;
-            score = 0;
-        }
-            this.response.speak(say).listen("I will repeat that again . . ."+say); 
-            this.emit(':responseReady');
-    },
-      "SessionEndedRequest": function () {
-          console.log(`Session ended in help state: ${this.event.request.reason}`);
-    },"TrueFactIntent": function () {//If true
-        memo = "You just answered true to the last question";
-        var lastQuestion = arrayPosition - 1;
-        var say;
-        if(answerArray[lastQuestion] == true){
-          var say = "That is correct, that was true";
-          score++;
-        }else{
-          var say = "That is incorrect, that was actually false";
-        }
-        this.response.speak(say); 
-        this.emit(':responseReady');
-    },"FalseFactIntent": function () {//If true
-        memo = "You just answered true to the last question";
-        var lastQuestion = arrayPosition - 1;
-        var say;
-        if(answerArray[lastQuestion] == false){
-          var say = "That is correct, that was false";
-          score++;
-        }else{
-          var say = "That is incorrect, that was actually true";
-        }
-        this.response.speak(say); 
-        this.emit(':responseReady');
-    },"ScoreIntent": function () {//If true
-        this.response.speak("your score is "+score); 
-        this.emit(':responseReady');
+      score = 0;
     }
-   };
-
-// This is the function that AWS Lambda calls every time Alexa uses your skill.
-exports.handler = function(event, context, callback){
-  // Set up the Alexa object
-  const alexa = Alexa.handler(event, context);
-    // Register Handlers
-    alexa.registerHandlers(handlers);
-    // Start our Alexa code
-    alexa.execute();
+    this.response.speak(say).listen("I will repeat that again . . ." + say);
+    this.emit(':responseReady');
+  },
+  "SessionEndedRequest": function() {
+    console.log(`Session ended in help state: ${this.event.request.reason}`);
+  },
+  "TrueFactIntent": function() { //If true
+    memo = "You just answered true to the last question";
+    var lastQuestion = arrayPosition - 1;
+    var say;
+    if (answerArray[lastQuestion] == true) {
+      var say = "That is correct, that was true";
+      score++;
+    } else {
+      var say = "That is incorrect, that was actually false";
+    }
+    this.response.speak(say);
+    this.emit(':responseReady');
+  },
+  "FalseFactIntent": function() { //If true
+    memo = "You just answered true to the last question";
+    var lastQuestion = arrayPosition - 1;
+    var say;
+    if (answerArray[lastQuestion] == false) {
+      var say = "That is correct, that was false";
+      score++;
+    } else {
+      var say = "That is incorrect, that was actually true";
+    }
+    this.response.speak(say);
+    this.emit(':responseReady');
+  },
+  "ScoreIntent": function() { //If true
+    this.response.speak("your score is " + score);
+    this.emit(':responseReady');
+  }
 };
 
-
+// This is the function that AWS Lambda calls every time Alexa uses your skill.
+exports.handler = function(event, context, callback) {
+  // Set up the Alexa object
+  const alexa = Alexa.handler(event, context);
+  // Register Handlers
+  alexa.registerHandlers(handlers);
+  // Start our Alexa code
+  alexa.execute();
+};
