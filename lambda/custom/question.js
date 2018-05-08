fd60cb28d4831e164e1b40c3f0a2f2d950169c08
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2018-05-08 18:55:34 +0100

    add corresponding context to questions and answers

--- lambda/custom/question.js
+++ lambda/custom/question.js
@@ -24,6 +24,10 @@ module.exports = {
     },
     returnAllContext: function(){
         const contextArray = [
+        "In the context of the Cold War the acronym MAD stood for Mutually Assured Destruction and meant that with the development of ever more powerful nuclear weapons, if either side were to use them it was sure to lead to the end of life on earth.",
+        "Sputnik was a USSR telecommunications satellite - the first man-made object in Space",
+        "An important aspect of the Cold War was the Space Race - the competition between the USA and USSR to take control of Space. After the USSR put the first man in Space in 1961, the US President John F Kennedy challenged Americans to put a man on the moon by the end of the decade, which they did in 1969.",
+        "The primary objective of NATO (North Atlantic Treaty Organisation) was to resist an attack by the USSR on the USA or its Allies in Western Europe based on the principle that an attack on one of its members was an attack on all of them."
 
         ];
         
