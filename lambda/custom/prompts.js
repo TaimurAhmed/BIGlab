d71752d0caedb30646438d3ad542d82e3fa9dce6
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2018-05-08 21:04:14 +0100

    replace concat with coma to seperate Strings

--- lambda/custom/prompts.js
+++ lambda/custom/prompts.js
@@ -5,7 +5,7 @@ module.exports = {
     returnAllQuesPrompts: function() {
         const promptArray = [
             "Is the statement true or false ? ",
-            "Is the statement correct or incorrect ? " +
+            "Is the statement correct or incorrect ? ",
             "Yes or no ? "
         ];
         return promptArray;
